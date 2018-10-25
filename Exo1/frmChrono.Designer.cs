a3e2aa5e443d7f5d399e38a37a88a745c3d81d3e
nicolas duval <nicolas.duval@maformationenligne.bzh>
2018-10-25 17:23:18 +0200

    chrono a faire

--- Exo1/frmChrono.Designer.cs
+++ Exo1/frmChrono.Designer.cs
@@ -31,6 +31,8 @@
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lbChrono = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -44,23 +46,43 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(125, 28);
+            this.textBox1.Location = new System.Drawing.Point(128, 27);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(125, 66);
+            this.textBox2.Location = new System.Drawing.Point(128, 65);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 2;
+            // 
+            // lbChrono
+            // 
+            this.lbChrono.AutoSize = true;
+            this.lbChrono.Location = new System.Drawing.Point(12, 30);
+            this.lbChrono.Name = "lbChrono";
+            this.lbChrono.Size = new System.Drawing.Size(47, 13);
+            this.lbChrono.TabIndex = 3;
+            this.lbChrono.Text = "Chrono :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nombre Aléatoire :";
             // 
             // frmChrono
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(442, 109);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbChrono);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.richTextBox1);
@@ -77,5 +99,7 @@
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lbChrono;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Exo1/frmMDI.cs
+++ Exo1/frmMDI.cs
@@ -25,93 +25,93 @@ namespace Exo1
 
 
 
-        private void ShowNewForm(object sender, EventArgs e)
-        {
-            Form childForm = new Form();
-            childForm.MdiParent = this;
-            childForm.Text = "Fenêtre " + childFormNumber++;
-            childForm.Show();
-        }
+        //private void ShowNewForm(object sender, EventArgs e)
+        //{
+        //    Form childForm = new Form();
+        //    childForm.MdiParent = this;
+        //    childForm.Text = "Fenêtre " + childFormNumber++;
+        //    childForm.Show();
+        //}
 
-        private void OpenFile(object sender, EventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            openFileDialog.Filter = "Fichiers texte (*.txt)|*.txt|Tous les fichiers (*.*)|*.*";
-            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
-            {
-                string FileName = openFileDialog.FileName;
-            }
-        }
+        //private void OpenFile(object sender, EventArgs e)
+        //{
+        //    OpenFileDialog openFileDialog = new OpenFileDialog();
+        //    openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        //    openFileDialog.Filter = "Fichiers texte (*.txt)|*.txt|Tous les fichiers (*.*)|*.*";
+        //    if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+        //    {
+        //        string FileName = openFileDialog.FileName;
+        //    }
+        //}
 
-        private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            saveFileDialog.Filter = "Fichiers texte (*.txt)|*.txt|Tous les fichiers (*.*)|*.*";
-            if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
-            {
-                string FileName = saveFileDialog.FileName;
-            }
-        }
+        //private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    SaveFileDialog saveFileDialog = new SaveFileDialog();
+        //    saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        //    saveFileDialog.Filter = "Fichiers texte (*.txt)|*.txt|Tous les fichiers (*.*)|*.*";
+        //    if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
+        //    {
+        //        string FileName = saveFileDialog.FileName;
+        //    }
+        //}
 
-        private void ExitToolsStripMenuItem_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
+        //private void ExitToolsStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    this.Close();
+        //}
 
-        private void CutToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-        }
+        //private void CutToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //}
 
-        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-        }
+        //private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //}
 
-        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-        }
-
-
+        //private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //}
 
 
-        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.Cascade);
-        }
 
-        private void TileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.TileVertical);
-        }
 
-        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.TileHorizontal);
-        }
+        //private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    LayoutMdi(MdiLayout.Cascade);
+        //}
 
-        private void ArrangeIconsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.ArrangeIcons);
-        }
+        //private void TileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    LayoutMdi(MdiLayout.TileVertical);
+        //}
 
-        private void CloseAllToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            foreach (Form childForm in MdiChildren)
-            {
-                childForm.Close();
-            }
-        }
+        //private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    LayoutMdi(MdiLayout.TileHorizontal);
+        //}
+
+        //private void ArrangeIconsToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    LayoutMdi(MdiLayout.ArrangeIcons);
+        //}
+
+        //private void CloseAllToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    foreach (Form childForm in MdiChildren)
+        //    {
+        //        childForm.Close();
+        //    }
+        //}
 
         private void frmMDI_Load(object sender, EventArgs e)
         {
 
         }
 
-        protected void MDIChildNew_Click(object sender, System.EventArgs e)
-        {
+        //protected void MDIChildNew_Click(object sender, System.EventArgs e)
+        //{
 
-        }
+        //}
 
         private void chronoToolStripMenuItem_Click(object sender, EventArgs e)
         {
